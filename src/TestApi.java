edbcc03b48a6eda3b08aa79ca8c3cf1ad7a5a8d4
21210112 <21210112@buaa.edu.cn>
2022-06-08 14:52:00 +0800

    Finished the console part

---
+++ src/TestApi.java
@@ -0,0 +1,25 @@
+import byteview.byteview;
+
+import java.util.HashMap;
+class testGetter implements IGetter{
+    static HashMap<String, String> map = new HashMap<>();
+    {
+        map.put("key1", "val2");
+        map.put("key2", "key2");
+    }
+    @Override
+    public byteview get(String key) {
+        return new byteview(map.get(key));
+    }
+}
+
+public class TestApi {
+    public static void main(String[] args) {
+        String[] addrs = new String[]{"http://localhost:8996", "http://localhost:8997", "http://localhost:8998"};
+        geecache group =  geecache.newGroup("test", new testGetter());
+        APIServer.startMainServer(0, group, addrs);
+        for (String addr : addrs) {
+            APIServer.startSubServer(addr, group);
+        }
+    }
+}
